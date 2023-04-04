80171d3cebac3b5f0e540791c19d2077b317c087
Shravan Rao <shravan.mrao@gmail.com>
2023-04-04 07:27:17 +0000

    Added event

--- repository.js
+++ repository.js
@@ -29,6 +29,7 @@ const mockEvents = {
     events: [
         { id: 1, title: 'a mock event', description: 'something really cool', location: 'Chez Joe Pizza', likes: 0, datetime_added: '2022-02-01:12:00' },
         { id: 2, title: 'another mock event', description: 'something even cooler', location: 'Chez John Pizza', likes: 0, datetime_added: '2022-02-01:12:00' },
+        { id: 3, title: 'a third mock event', description: 'mind blowing', location: 'Pizza Hut', likes: 23, datetime_added: '2023-02-01:12:00' },
     ]
 };
 
